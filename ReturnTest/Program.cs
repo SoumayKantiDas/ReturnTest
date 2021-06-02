d6555764efa91501c2be929e426c8264ae8206f7
Soumay kanti Das <anikds2859@outlook.com>
2021-06-02 22:08:26 +0600

    Add project files.

---
+++ ReturnTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ReturnTest
+{
+    class ReturnTest
+    {
+        static double CalculateArea(int r)
+        {
+           
+            double area = r * r * Math.PI;
+            return area;
+        }
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int radious = int.Parse(s);
+            Console.WriteLine($"The area is {CalculateArea(radious)}");
+           
+        }
+    }
+}
